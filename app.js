79b812dab1fd83106bac8816e95ab76465ae2de3
dreigonlol <ivanlolmendoza@gmail.com>
2024-12-31 18:16:06 -0600

    Columnas correctas

--- app.js
+++ app.js
@@ -7,10 +7,51 @@ const assetsURL = "./MAssets.json";
 
 const attributeMapping = {
     "A/C Compressor": ["Tipo de Refrigerante", "Ranuras de Polea", "Diametro de Polea", "Voltaje de bobina"],
-    "Radiator": ["Alto cm", "Ancho cm", "Espesor mm", "Alto in", "Ancho in", "Espesor in"],
+    "Radiator": ["Alto in", "Ancho in", "Espesor cm"],
+    "A/C Condenser": ["Alto in", "Ancho in", "Espesor cm"],
+    "A/C Condenser Fan": ["Ventiladores", "Aspas", "Pines"],
+    "A/C Evaporator Core": ["Alto in", "Ancho in", "Espesor mm"],
+    "Alternator": [],
+    "Engine Coolant Reservoir": ["Ancho mm", "Fondo mm", "Largo mm", "C/Tapon", "C/Sensor"],
+    "Engine Coolant Reservoir Cap": [],
+    "Engine Cooling Fan": ["Ventiladores", "Aspas","Tipo de Conector"],
+    "Engine Cooling Fan Blade": ["Ventiladores", "Aspas"],
+    "Engine Cooling Fan Clutch": ["Tipo de embrague", "Rotación del embrague","Diametro del embrague"],
+    "Engine Cooling Fan Shroud": [],
+    "Engine Oil Cooler": ["Largo Pulgadas", "Alto Pulgadas", "Ancho Pulgadas"],
+    "HVAC Blower Motor": [],
+    "HVAC Heater Core": ["Alto in", "Ancho in", "Espesor mm"],
+    "HVAC Heater Hose": [],
+    "Intercooler": ["Alto in", "Ancho in", "Espesor mm"],
+    "Power Steering Reservoir": [],
+    "Transmission Oil Cooler": ["Alto in", "Largo in"],
+    "Washer Fluid Reservoir": []
     // Agrega más productos según sea necesario
 };
 
+const selectedColumnsMapping = {
+    "A/C Compressor": ["C1", "C2", "C3", "C4"],
+    "Radiator": ["C5", "C6", "C3"],
+    "A/C Condenser": ["C4", "C5", "C3"],
+    "A/C Condenser Fan": ["C1", "C2","C8"],
+    "A/C Evaporator Core": ["C1", "C2","C3"],
+    "Alternator": [],
+    "Engine Coolant Reservoir": ["C1", "C2", "C3", "C5", "C6"],
+    "Engine Coolant Reservoir Cap": [],
+    "Engine Cooling Fan": ["C1", "C2","C8"],
+    "Engine Cooling Fan Blade": ["C1", "C2"],
+    "Engine Cooling Fan Clutch": ["C1", "C2","C3"],
+    "Engine Cooling Fan Shroud": [],
+    "Engine Oil Cooler": ["C2", "C3", "C4"],
+    "HVAC Blower Motor": [],
+    "HVAC Heater Core": ["C4", "C5", "C3"],
+    "HVAC Heater Hose": [],
+    "Intercooler": ["C4", "C5", "C3"],
+    "Power Steering Reservoir": [],
+    "Transmission Oil Cooler": ["C2", "C3"],
+    "Washer Fluid Reservoir": []
+    // Agrega más configuraciones según sea necesario
+};
 
 // Función para cargar JSON
 async function loadJSON(url) {
@@ -60,22 +101,22 @@ async function renderCatalogAsTables() {
         var j=0;
         for (const [productType, products] of Object.entries(groupedByProductType)) {
             const attributes = attributeMapping[productType] || ["C1", "C2", "C3", "C4", "C5", "C6"];
+            const selectedColumns = selectedColumnsMapping[productType] || ["C1", "C2", "C3", "C4", "C5", "C6"];
 
             let sectionHTML = `
                 <div class="product-section">
-                    <h3>${productType}</h3>
+                    <div id="Linea" class="Linea"><h3>${productType}</h3></div>
                     <table>
                         <thead>
                             <tr>
-                                <th>Part Number</th>
+                                <th id="TColumnas" class="TColumnas">Part Number</th>
                                 <th id="year" class="year">Año</th>
-                                ${attributes.map(attr => `<th>${attr}</th>`).join('')}
+                                ${attributes.map(attr => `<th id="TColumnas" class="TColumnas">${attr}</th>`).join('')}
                             </tr>
                         </thead>
                         <tbody>
             `;
 
-            // Agrupar compatibilidades por marca y modelo
             const groupedCompatibilities = compatibilities.reduce((acc, compatibility) => {
                 const { Make, Model, Year, PartNumber } = compatibility;
                 if (!acc[Make]) {
@@ -88,41 +129,31 @@ async function renderCatalogAsTables() {
                 return acc;
             }, {});
 
-            // Recorre cada marca
+            
             for (const [make, models] of Object.entries(groupedCompatibilities)) {
-                
                 i=0;
-                // Recorre cada modelo dentro de una marca
+
                 for (const [model, compatibilitiesForModel] of Object.entries(models)) {
                     j=0;
-                    // Agregar filas para cada compatibilidad de ese modelo
+                    
                     compatibilitiesForModel.forEach(({ Year, PartNumber }) => {
                         const product = products.find(p => p.PartNumber === PartNumber);
+                        
                         if (product) {
                             if(i==0){
-                                sectionHTML += `
-                                    <tr class="subheading">
-                                        <td colspan="100%" id="Marca" class="Marca">${make}</td>
-                                    </tr>
-                                `;
-                                i=1;
+                                sectionHTML += `<tr class="subheading"><td colspan="100%" id="Marca" class="Marca">${make}</td></tr>`;
+                                i++;
                             }
-
                             if(j==0){
-                                sectionHTML += `
-                                    <tr class="subheading">
-                                        <td colspan="100%" id="Modelo" class="Modelo">${model}</td>
-                                    </tr>
-                                `;
-                                j=1;
+                                sectionHTML += `<tr class="subheading"><td colspan="100%" id="Modelo" class="Modelo">${model}</td></tr>`;
+                                j++;
                             }
-
                             sectionHTML += `
-                                <tr>
+                                <tr id="FT" class="FT">
                                     <td>${PartNumber}</td>
                                     <td>${Year}</td>
-                                    ${attributes.map((attr, index) => {
-                                        const value = product[`C${index + 1}`];
+                                    ${selectedColumns.map(column => {
+                                        const value = product[column];
                                         return `<td>${value || 'N/A'}</td>`;
                                     }).join('')}
                                 </tr>
@@ -132,12 +163,7 @@ async function renderCatalogAsTables() {
                 }
             }
 
-            sectionHTML += `
-                        </tbody>
-                    </table>
-                </div>
-            `;
-
+            sectionHTML += `</tbody></table></div>`;
             catalogContainer.innerHTML += sectionHTML;
         }
     } catch (error) {
